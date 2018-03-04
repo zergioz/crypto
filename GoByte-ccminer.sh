92125b22eadee012323b77adc413a67f5b1d9962
Zergioz <serrod@gmail.com>
2018-03-04 17:41:42 -0500

    changed port for to a lowe diff level

--- GoByte-ccminer.sh
+++ GoByte-ccminer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./ccminer/ccminer -a neoscrypt -d 0,1  -o stratum+tcp://pool.unimining.net:4236 -u GPViTf8oH9dXkWcofxFmn9C1TH2ZM5i9ZH -p c=GBX
+./ccminer/ccminer -a neoscrypt -d 0,1  -o stratum+tcp://pool.unimining.net:2053 -u GPViTf8oH9dXkWcofxFmn9C1TH2ZM5i9ZH -p c=GBX
