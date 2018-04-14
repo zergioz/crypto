d4c2ce28d60f110247e81f9914bc6a0f6fa8c3af
Zergioz <serrod@gmail.com>
2018-04-14 10:54:46 -0400

    modified GoByte ccminer script to allocate 2 cards instead of a full rack

--- GoByte-ccminer.sh
+++ GoByte-ccminer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./ccminer/ccminer -a neoscrypt -d 0,1  -o stratum+tcp://pool.unimining.net:4233 -u GPViTf8oH9dXkWcofxFmn9C1TH2ZM5i9ZH -p c=GBX
+./ccminer/ccminer -a neoscrypt -d 0,1  -o stratum+tcp://neoscrypt.eu1.unimining.net:4236 -u GPViTf8oH9dXkWcofxFmn9C1TH2ZM5i9ZH -p c=GBX
